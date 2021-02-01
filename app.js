751fb0a17e317235a953851eabf472e2b88f0f4f
tphuong141607 <tphuong141607@gmail.com>
2021-02-01 16:25:13 -0500

    updated package.json

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.get('/about', (req, res)=> {
 });
 
 
-const port = 3000; 
+const port = process.env.PORT || 3000; 
 app.listen(port, () => {
     console.log(`App running on port ${port}...`);
 });
