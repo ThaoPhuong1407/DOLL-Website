7bf366e1ddd972e09509dd6b514a2713f64d622e
tphuong141607 <tphuong141607@gmail.com>
2021-02-01 15:43:25 -0500

    website

---
+++ app.js
@@ -0,0 +1,32 @@
+const express   = require('express');
+const path      = require('path');
+
+// Add a bunch of functions from module express to the variable `app`
+const app = express();
+
+//  Setting up pug templates
+app.set('view engine', 'pug'); // Express framework supports pug template
+app.set('views', path.join(__dirname, 'views'));
+
+// Serving static files from the public folders
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', (req, res)=> {
+    res.status(200).render('homepage', {
+        test: 'Thao Phuong',
+    });
+});
+
+app.get('/about', (req, res)=> {
+    res.status(200).render('about', {
+        test: 'Thao Phuong',
+    });
+});
+
+
+const port = 3000; 
+app.listen(port, () => {
+    console.log(`App running on port ${port}...`);
+});
+
