dbf88b273c6fec08d75d9d2a12b3218268b60a6b
tphuong141607 <tphuong141607@gmail.com>
2021-02-10 08:03:08 -0500

    Redo front-end

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', (req, res)=> {
-    res.status(200).render('homepage', {
+    res.status(200).render('home', {
         test: 'Thao Phuong',
     });
 });
